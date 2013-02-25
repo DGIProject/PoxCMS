ae173bd9524b174f302c6c703675f349bfe0b796
Villena Guillaume <villenagui.free.fr@gmail.com>
2013-02-25 16:18:10 +0100

    starting design for the admin panel and create a phpstorm project

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,4 @@
 <?php
-
 include "function.php";
 echo $_SESSION['pseudo'];
 if ($_SESSION['pseudo'] != NULL)
@@ -16,22 +15,60 @@ else
 }
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
+<!DOCTYPE html>
+<html>
     <head>
         <title>page d'administration - Connection</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-        <link href="vue/blog/style.css" rel="stylesheet" type="text/css" /> 
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/bootstrap-responsive.min.css" />
+        <link rel="stylesheet" href="css/unicorn.login.css" />
     </head>
-        
     <body>
-        <h1>Interface d'adminitration</h1>
-        <p><form action="" method="post">
-            <label for="identutil">nom d'utilisateur</label> <input type="text" name="ident_utilisateur"id="identutil"><br>
-            <label for="passutil">mot de passe</label> <input type="password" name="pass_utilisateur" id="passutil"><br>
-            <input type="submit" name="co">
-        </form> </p>
+    <div id="logo">
+        <img src="img/logo.png" alt="" />
+    </div>
+        <div id="loginbox">
+            <form id="loginform" class="form-vertical" action="">
+                <p>Enter username and password to continue.</p>
+                <div class="control-group">
+                    <div class="controls">
+                        <div class="input-prepend">
+                            <span class="add-on"><i class="icon-user"></i></span><input type="text" name="ident_utilisateur" placeholder="Username" />
+                        </div>
+                    </div>
+                </div>
+                <div class="control-group">
+                    <div class="controls">
+                        <div class="input-prepend">
+                            <span class="add-on"><i class="icon-lock"></i></span><input type="password" name="pass_utilisateur" placeholder="Password" />
+                        </div>
+                    </div>
+                </div>
+                <div class="form-actions">
+                    <span class="pull-left"><a href="#" class="flip-link" id="to-recover">Lost password?</a></span>
+                    <span class="pull-right"><input type="submit" class="btn btn-inverse" value="Login" /></span>
+                </div>
+            </form>
+            <form id="recoverform" action="#" class="form-vertical">
+                <p>Enter your e-mail address below and we will send you instructions how to recover a password.</p>
+                <div class="control-group">
+                    <div class="controls">
+                        <div class="input-prepend">
+                            <span class="add-on"><i class="icon-envelope"></i></span><input type="text" placeholder="E-mail address" />
+                        </div>
+                    </div>
+                </div>
+                <div class="form-actions">
+                    <span class="pull-left"><a href="#" class="flip-link" id="to-login">&lt; Back to login</a></span>
+                    <span class="pull-right"><input type="submit" class="btn btn-inverse" value="Recover" /></span>
+                </div>
+            </form>
+        </div>
 
+            <script src="js/jquery.min.js"></script>
+            <script src="js/unicorn.login.js"></script>
 
 <?php
 echo $result;
