128401149bdc304c7efc918642d15fda9b639aef
Villena Guillaume <villenagui.free.fr@gmail.com>
2013-03-05 22:05:37 +0100

    linking filemanagment.php with other pages in admin panel

--- admin/edit_page.php
+++ admin/edit_page.php
@@ -34,7 +34,7 @@ include "function.php";
         <div class="btn-group">
             <a class="btn btn-large tip-bottom" title="Manage Pages" href="pages.php"><i class="icon-file"></i></a>
             <a class="btn btn-large tip-bottom" title="Manage Users" href="users.php"><i class="icon-user"></i></a>
-            <a class="btn btn-large tip-bottom" title="Manage Files"><i class="icon-folder-open"></i></a>
+            <a class="btn btn-large tip-bottom" title="Manage Files" href="filemanagment.php"><i class="icon-folder-open"></i></a>
         </div>
     </div>
     <div id="breadcrumb">
--- admin/users.php
+++ admin/users.php
@@ -27,7 +27,7 @@ if ($_SESSION['pseudo'] == null) {
         <div class="btn-group">
             <a class="btn btn-large tip-bottom" title="Manage Pages" href="pages.php"><i class="icon-file"></i></a>
             <a class="btn btn-large tip-bottom" title="Manage Users" href="users.php"><i class="icon-user"></i></a>
-            <a class="btn btn-large tip-bottom" title="Manage Files"><i class="icon-folder-open"></i></a>
+            <a class="btn btn-large tip-bottom" title="Manage Files" href="filemanagment.php"><i class="icon-folder-open"></i></a>
         </div>
     </div>
     <div id="breadcrumb">
